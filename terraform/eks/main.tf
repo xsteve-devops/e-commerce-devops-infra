8936e0a81d7dec30c1392296961fc2ab107ef97f
steveyang <steveyang128@yahoo.com>
2026-06-15 00:27:10 +0900

    infra: configure EKS addons for node networking

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -37,8 +37,18 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name                                     = var.cluster_name
-  kubernetes_version                       = var.cluster_version
+  name               = var.cluster_name
+  kubernetes_version = var.cluster_version
+
+  addons = {
+    vpc-cni = {
+      before_compute = true
+    }
+
+    kube-proxy = {}
+
+    coredns = {}
+  }
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
