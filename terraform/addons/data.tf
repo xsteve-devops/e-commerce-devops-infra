bae96e0fc4c818864f09ca928a11f9f899acbb8b
steveyang <steveyang128@yahoo.com>
2026-06-19 23:53:20 +0900

    infra: add AWS Load Balancer Controller addon

--- terraform/addons/data.tf
+++ terraform/addons/data.tf
@@ -2,12 +2,12 @@ data "terraform_remote_state" "eks" {
   backend = "s3"
 
   config = {
-    bucket = "e-commercial-devops-project-remote-state"
-    key    = "e-commerce-devops/eks/terraform.tfstate"
-    region = "ap-northeast-1"
-    encrypt = true
+    bucket       = "e-commercial-devops-project-remote-state"
+    key          = "e-commerce-devops/eks/terraform.tfstate"
+    region       = "ap-northeast-1"
+    encrypt      = true
     use_lockfile = true
-  }   
+  }
 }
 
 data "aws_eks_cluster" "this" {
--- terraform/addons/main.tf
+++ terraform/addons/main.tf
@@ -1,3 +1,5 @@
+# Argo CD installation
+
 resource "helm_release" "argocd" {
   name             = "argocd"
   namespace        = "argocd"
@@ -8,3 +10,69 @@ resource "helm_release" "argocd" {
 
 }
 
+# AWS Load Balancer Controller IAM resources
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "aws-load-balancer-controller"
+  policy = file("${path.module}/policies/aws-load-balancer-controller-iam-policy.json")
+}
+
+resource "aws_iam_role" "alb_controller" {
+  name = "aws-load-balancer-controller"
+  assume_role_policy = jsonencode({
+
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          Federated = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+        }
+        "Condition" : {
+          "StringEquals" : {
+            "${data.terraform_remote_state.eks.outputs.oidc_provider}:aud" : "sts.amazonaws.com",
+            "${data.terraform_remote_state.eks.outputs.oidc_provider}:sub" : "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          }
+        }
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+# Application Load Balancer Controller installation
+
+resource "helm_release" "alb_controller" {
+  name             = "aws-load-balancer-controller"
+  namespace        = "kube-system"
+  create_namespace = false
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+
+  values = [
+    yamlencode({
+      clusterName = data.terraform_remote_state.eks.outputs.cluster_name
+      region      = var.aws_region
+      vpcId       = data.terraform_remote_state.eks.outputs.vpc_id
+
+      serviceAccount = {
+        create = true
+        name   = "aws-load-balancer-controller"
+        annotations = {
+          "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
+        }
+      }
+    })
+  ]
+  depends_on = [
+    aws_iam_role_policy_attachment.alb_controller
+  ]
+
+}
